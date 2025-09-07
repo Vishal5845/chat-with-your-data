cdac63c1cd9da4a605d815e6afa0480911acc1df
Vishal Grover <vishalgrover@Vishals-MacBook-Air.local>
2025-09-07 23:24:00 +0530

    Day 1: setup + loaded sample dataset into DuckDB

---
+++ load_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import duckdb as ddb
+
+# Read the csv file into a pandas DataFrame
+df = pd.read_csv('data/data.csv', encoding='latin1')
+
+# Connect to DuckDB (in-memory)
+condb = ddb.connect("ai_assistant.duckdb")
+
+
+# Create table
+condb.execute("""
+    CREATE TABLE IF NOT EXISTS ecommerce as select * from df
+""")
+
+# Verify the table creation
+result = condb.execute("SELECT * FROM ecommerce").fetchall()
+print("Total rows in ecommerce table:", result[0][0])
+print("Data loaded into DuckDB successfully.")
