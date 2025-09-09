95c8456d192f5d7f25745dfde319b9ac7e917694
Vishal Grover <vishalgrover2001@gmail.com>
2025-09-09 23:52:16 +0530

    Day 3: Added deeper EDA (orders, customers, top products, sales trend)

--- src/eda.py
+++ src/eda.py
@@ -27,3 +27,31 @@ print(con.execute("""select country, round(sum(unitprice * quantity), 2) as tota
                   order by total_revenue desc
                   limit 5
                   """).fetchdf())
+
+#4. Total number of orders
+print("\n 4. Total number of unique orders:")
+print(con.execute("""select count(distinct invoiceno) from ecommerce""").fetchone()[0])
+
+#5. Number of unique customers
+print("\n 5. Total number of unique customers:")
+print(con.execute(""" select count(distinct customerid) from ecommerce""").fetchone()[0])
+
+#6. Top 5 products
+print("\n 6. Top 5 products by quantity sold:")
+print(con.execute("""
+                  select description, sum(quantity) as total_quantity
+                  from ecommerce
+                  group by description
+                  order by total_quantity desc
+                  limit 5
+                  """).fetch_df())
+
+#7. Monthly sales trend
+print("\n 7. Monthly sales trend (total revenue per month):")
+print(con.execute("""
+                  select strftime(strptime(invoicedate, '%m/%d/%Y %H:%M'), '%Y-%m') as Month, sum(quantity * unitprice) as total_revenue
+                  from ecommerce
+                  group by Month
+                  order by total_revenue
+                  """).fetch_df())
+
